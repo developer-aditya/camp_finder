0b6c2e9d3d74b221a7e19b913c3d599e0865ac65
Aditya Singh <adityathedev@gmail.com>
2021-06-08 23:27:55 +0530

    lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import './webfonts-with-css/css/all.min.css';
 
 import Navbar from './components/layout/Navbar';
 import Home from './components/Home';
-import Bootcamps from './components/bootcamps/Bootcamps';
 import SignIn from './components/modals/SignIn';
 import SignUp from './components/modals/SignUp';
-import SingleBootcamp from './components/bootcamps/SingleBootcamp';
-
-import BootcampForm from './components/bootcamps/BootcampForm';
-import CourseForm from './components/course/CourseForm';
-import ManageBootcamp from './components/bootcamps/ManageBootcamp';
-import ManageReview from './components/reviews/ManageReview';
-import ManageAccount from './components/account/ManageAccount';
-import ChangePassword from './components/account/ChangePassword';
-import ResetPassword from './components/account/ResetPassword';
 import ForgotPassword from './components/modals/ForgotPassword';
 
 import store from './store';
@@ -25,6 +15,22 @@ import { userGet } from './actions/authAction';
 
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import M from '../node_modules/materialize-css/dist/js/materialize.min';
+
+const Bootcamps = lazy(() => import('./components/bootcamps/Bootcamps'));
+const SingleBootcamp = lazy(() =>
+	import('./components/bootcamps/SingleBootcamp'),
+);
+const BootcampForm = lazy(() => import('./components/bootcamps/BootcampForm'));
+const CourseForm = lazy(() => import('./components/course/CourseForm'));
+const ManageBootcamp = lazy(() =>
+	import('./components/bootcamps/ManageBootcamp'),
+);
+const ManageReview = lazy(() => import('./components/reviews/ManageReview'));
+const ManageAccount = lazy(() => import('./components/account/ManageAccount'));
+const ChangePassword = lazy(() =>
+	import('./components/account/ChangePassword'),
+);
+const ResetPassword = lazy(() => import('./components/account/ResetPassword'));
 
 const App = () => {
 	useEffect(() => {
@@ -50,46 +56,70 @@ const App = () => {
 					<ForgotPassword />
 					<Switch>
 						<Route exact path='/' component={Home}></Route>
-						<Route exact path='/bootcamps' component={Bootcamps}></Route>
-						<Route
-							path='/singleBootcamp'
-							component={SingleBootcamp}
-						></Route>
-						<Route
-							exact
-							path='/manageBootcamp'
-							component={ManageBootcamp}
-						></Route>
-						<Route
-							exact
-							path='/manageBootcamp/bootcampForm'
-							component={BootcampForm}
-						></Route>
-						<Route
-							exact
-							path='/manageBootcamp/courseForm'
-							component={CourseForm}
-						></Route>
-						<Route
-							exact
-							path='/manageReview'
-							component={ManageReview}
-						></Route>
-						<Route
-							exact
-							path='/manageAccount'
-							component={ManageAccount}
-						></Route>
-						<Route
-							exact
-							path='/manageAccount/changePassword'
-							component={ChangePassword}
-						></Route>
-						<Route
-							exact
-							path='/manageAccount/resetPassword/:resetToken'
-							component={ResetPassword}
-						></Route>
+						<Suspense
+							fallback={
+								<div
+									className='progress'
+									style={{
+										position: 'absolute',
+										top: '50%',
+										left: '25%',
+										width: '50%',
+										backgroundColor: '#c0e7fa',
+									}}
+								>
+									<div
+										className='indeterminate'
+										style={{ backgroundColor: '#1c9fe0' }}
+									></div>
+								</div>
+							}
+						>
+							<Route
+								exact
+								path='/bootcamps'
+								component={Bootcamps}
+							></Route>
+							<Route
+								path='/singleBootcamp'
+								component={SingleBootcamp}
+							></Route>
+							<Route
+								exact
+								path='/manageBootcamp'
+								component={ManageBootcamp}
+							></Route>
+							<Route
+								exact
+								path='/manageBootcamp/bootcampForm'
+								component={BootcampForm}
+							></Route>
+							<Route
+								exact
+								path='/manageBootcamp/courseForm'
+								component={CourseForm}
+							></Route>
+							<Route
+								exact
+								path='/manageReview'
+								component={ManageReview}
+							></Route>
+							<Route
+								exact
+								path='/manageAccount'
+								component={ManageAccount}
+							></Route>
+							<Route
+								exact
+								path='/manageAccount/changePassword'
+								component={ChangePassword}
+							></Route>
+							<Route
+								exact
+								path='/manageAccount/resetPassword/:resetToken'
+								component={ResetPassword}
+							></Route>
+						</Suspense>
 						<Route
 							path='*'
 							render={(props) => (
